aa81d041d2aea278db54e21ec05e66bcc3ad6b67
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-07 22:33:22 +0200

    Début imp Socket.IO

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var io = require('socket.io')(8080);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 io.on('connection', function (socket) {
-  socket.on('ferret', function (name, fn) {
-    fn('woot');
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
