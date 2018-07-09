788ed23638cc4eddfeea2b832ff001e750a25ae9
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-09 18:56:48 +0200

    Résolution d'un bug

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const server = require('http').createServer();
 
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
+console.log('Serveur créé !');
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
 });
-
 
 server.listen(8080);
