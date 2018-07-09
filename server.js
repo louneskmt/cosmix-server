cc4dc2d50e1092a47f7596616e31423158c204f9
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-09 20:18:41 +0200

    Tests pour que ça marche mdr

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const server = require('http').createServer();
 
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 console.log('Serveur créé !');
+
+server.listen(8080);
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
 });
-
-
-server.listen(8080);
