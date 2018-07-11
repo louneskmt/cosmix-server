90ef04fdabd485cabb0d980ed8475b0b934a0b3a
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 14:06:15 +0200

    Ajout de l'écriture dans un fichier

--- arduino.js
+++ arduino.js
@@ -1,5 +1,8 @@
 console.log("Lancé ! Récupération des données en cours...");
 
+var fs = require("fs");
+fs.writeFileSync("./lastest_experiment.csmx", "Mesure", "UTF-8");
+
 var SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 var port = new SerialPort('/dev/ttyACM0', {
@@ -12,6 +15,7 @@ port.on('open', function() {
 
     parser.on('data', function(data) {
         console.log(data);
+        fs.appendFileSync('./lastest_experiment.csmx', data);
     });
 });
 
