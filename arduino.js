9599ab0bfcd9f11752c5535124e0597fc5bd6497
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 18:29:44 +0200

    Ajout de commentaires

--- arduino.js
+++ arduino.js
@@ -13,25 +13,29 @@ var fs = require("fs");
 var now = new Date();
 var file_name = "./experiments_files/Experiment-" + now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() + "-" + now.getHours() + "-" + now.getMinutes() + ".csmx";
 console.log("Fichier de mesure enregistré sous : " + file_name);
-fs.writeFileSync(file_name, "Mesure\n", 'UTF-8');
+fs.writeFileSync(file_name, "Mesure\n", 'UTF-8'); 
 
+// Initialisation du module de récupération des données sur le port Serial
 var SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 var port = new SerialPort('/dev/ttyACM0', {
     baudRate: 9600
 });
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+const parser = port.pipe(new Readline({ delimiter: '\r\n' })); // Code afin d'obtenir les messages en clair et non le buffer
 
+// Evenement : port ouvert
 port.on('open', function() {
-    console.log('Serial Port Opened');
+    console.log('Serial Port Opened'); // Information console de l'ouverture du port
 
+    // Récupération des données disponibles et écriture à la fin du fichier
     parser.on('data', function(data) {
         console.log(data);
-        fs.appendFileSync(file_name, data);
-        fs.appendFileSync(file_name, '\n');
+        fs.appendFileSync(file_name, data); // Ajout de la ligne de mesure récupérée à la fin du fichier
+        fs.appendFileSync(file_name, '\n');  
     });
 });
 
+// Gestion des erreurs
 port.on('error', function(err) {
     console.log('Error: ', err.message);
 });
