0f909181e96085ce651a3cae35159b8ac00aa279
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 09:53:25 +0200

    Modification variable port

--- arduino.js
+++ arduino.js
@@ -1,17 +1,16 @@
 console.log("Lancé ! Récupération des données en cours...");
 
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyACM0', {
-    baudRate: 9600
-});
+var port = new SerialPort('/dev/ttyACM0');
 
 port.on('open', function() {
     console.log('Serial Port Opened');
     port.on('data', function(data) {
+        // console.log(data);
         console.log(data[0]);
     });
 });
 
 port.on('error', function(err) {
     console.log('Error: ', err.message);
-  })
+});
