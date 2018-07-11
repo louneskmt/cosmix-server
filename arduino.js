04d7e4b6495e93274df034650c0341ed8f54ed49
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 09:48:55 +0200

    Ajout gestion d'erreur d'ouverture de port

--- arduino.js
+++ arduino.js
@@ -1,7 +1,9 @@
 console.log("Lancé ! Récupération des données en cours...");
 
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyACM0');
+var port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600
+});
 
 port.on('open', function() {
     console.log('Serial Port Opened');
@@ -9,3 +11,7 @@ port.on('open', function() {
         console.log(data[0]);
     });
 });
+
+port.on('error', function(err) {
+    console.log('Error: ', err.message);
+  })
