805b1951607bc2309fb5b94b456a7a33111a41e1
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 13:33:39 +0200

    Test d'une autre méthode de lecture Serial

--- arduino.js
+++ arduino.js
@@ -5,9 +5,16 @@ var port = new SerialPort('/dev/ttyACM0');
 
 port.on('open', function() {
     console.log('Serial Port Opened');
+
+    /*
     port.on('data', function(data) {
         // console.log(data);
         console.log(data[0]);
+    });
+    */
+
+    port.on('readable', function() {
+        console.log(port.read());
     });
 });
 
