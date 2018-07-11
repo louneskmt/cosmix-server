161f51ea63e5430677d9af3ada234c7a1434e53c
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-07-11 13:43:29 +0200

    Correction erreur 2

--- arduino.js
+++ arduino.js
@@ -3,7 +3,7 @@ console.log("Lancé ! Récupération des données en cours...");
 var SerialPort = require('serialport');
 var port = new SerialPort('/dev/ttyACM0', {
     baudRate: 9600,
-    parser: SerialPort.parsers.readline("\n")
+    parser: SerialPort.parser-readline("\n")
 });
 
 port.on('open', function() {
