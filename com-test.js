55c9b6bd08082d29367e271d50ccd5cd1d196606
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-10-02 19:11:21 +0400

    Changement random coinc

--- com-test.js
+++ com-test.js
@@ -106,7 +106,7 @@ io.sockets.on('connection', function (socket) {
         var data = {
             newEventsC1: Math.floor(Math.random() * 10),
             newEventsC2: Math.floor(Math.random() * 10),
-            newCoincidence: Math.floor(Math.random() * 6),
+            newCoincidence: Math.floor(Math.random() * 4),
             status: 'ON'
         }
         socket.emit('newData', JSON.stringify(data));
