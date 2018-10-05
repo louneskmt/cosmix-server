c808080dac4bfb2e1944866ab6c50b14b6b0204c
lounes-kmt <38882571+lounes-kmt@users.noreply.github.com>
2018-10-05 22:42:44 +0400

    Modif script

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd ./cosmix-server/
 echo "Récupération des dernières modifications..."
+echo \n
 git pull
 killall node
-echo "Démarrage...\n"
+echo \n
+echo "Démarrage..."
+echo \n
 node com-test.js
